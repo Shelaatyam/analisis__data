3709f1a5ad3096bd8d01d6eddef2fa0eea5a28fe
Shelaatyam <161755754+Shelaatyam@users.noreply.github.com>
2024-03-01 01:57:58 +0700

    Update bike_dashboard.py

--- bike_dashboard.py
+++ bike_dashboard.py
@@ -3,12 +3,6 @@
 
 # ## DASHBOARD 
 
-# In[1]:
-
-
-# In[2]:
-
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -19,10 +13,6 @@ bike_data = pd.read_csv('bike_data_bersih.csv')
 # Menghitung rata-rata penggunaan sepeda per hari dan per minggu
 average_daily_users = bike_data['cnt'].mean()
 average_weekly_users = average_daily_users * 7
-
-# Menghitung rata-rata penggunaan sepeda pada hari kerja dan hari libur
-average_usage_workday = bike_data[bike_data['workingday'] == 1]['cnt'].mean()
-average_usage_holiday = bike_data[bike_data['workingday'] == 0]['cnt'].mean()
 
 # Menghitung rata-rata penggunaan sepeda per hari dalam seminggu
 average_daily_usage = bike_data.groupby('weekday')['cnt'].mean().reset_index()
@@ -42,12 +32,24 @@ st.plotly_chart(fig)
 
 # Pertanyaan 2: Pola Penggunaan Sepeda
 st.header('Pertanyaan 2: Pola Penggunaan Sepeda')
+
+# Menghitung rata-rata penggunaan sepeda pada hari kerja dan hari libur
+average_usage_workday = bike_data[bike_data['workingday'] == 1]['cnt'].mean()
+average_usage_holiday = bike_data[bike_data['workingday'] == 0]['cnt'].mean()
+
+# Menampilkan rata-rata penggunaan sepeda pada hari kerja dan hari libur
 st.write('Rata-rata Penggunaan Sepeda pada Hari Kerja: ', average_usage_workday)
 st.write('Rata-rata Penggunaan Sepeda pada Hari Libur: ', average_usage_holiday)
 
+# Visualisasi pola penggunaan sepeda antara hari kerja dan hari libur
+st.subheader('Perbandingan Pola Penggunaan Sepeda antara Hari Kerja dan Hari Libur')
 
-# In[ ]:
+# Membuat DataFrame untuk visualisasi
+usage_comparison = pd.DataFrame({
+    'Hari': ['Hari Kerja', 'Hari Libur'],
+    'Rata-rata Penggunaan Sepeda': [average_usage_workday, average_usage_holiday]
+})
 
-
-
-
+# Membuat bar chart
+fig2 = px.bar(usage_comparison, x='Hari', y='Rata-rata Penggunaan Sepeda', title='Perbandingan Pola Penggunaan Sepeda antara Hari Kerja dan Hari Libur')
+st.plotly_chart(fig2)
