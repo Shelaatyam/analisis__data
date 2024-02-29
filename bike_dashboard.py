09edd1e74deb95870a6c576f62e399dff8e9efeb
Shelaatyam <161755754+Shelaatyam@users.noreply.github.com>
2024-03-01 02:01:30 +0700

    Update bike_dashboard.py

--- bike_dashboard.py
+++ bike_dashboard.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ## DASHBOARD 
-
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 # Memuat data dari file CSV
 bike_data = pd.read_csv('bike_data_bersih.csv')
@@ -12,6 +9,10 @@ bike_data = pd.read_csv('bike_data_bersih.csv')
 # Menghitung rata-rata penggunaan sepeda per hari dan per minggu
 average_daily_users = bike_data['cnt'].mean()
 average_weekly_users = average_daily_users * 7
+
+# Menghitung rata-rata penggunaan sepeda pada hari kerja dan hari libur
+average_usage_workday = bike_data[bike_data['workingday'] == 1]['cnt'].mean()
+average_usage_holiday = bike_data[bike_data['workingday'] == 0]['cnt'].mean()
 
 # Menghitung rata-rata penggunaan sepeda per hari dalam seminggu
 average_daily_usage = bike_data.groupby('weekday')['cnt'].mean().reset_index()
@@ -31,12 +32,6 @@ st.plotly_chart(fig)
 
 # Pertanyaan 2: Pola Penggunaan Sepeda
 st.header('Pertanyaan 2: Pola Penggunaan Sepeda')
-
-# Menghitung rata-rata penggunaan sepeda pada hari kerja dan hari libur
-average_usage_workday = bike_data[bike_data['workingday'] == 1]['cnt'].mean()
-average_usage_holiday = bike_data[bike_data['workingday'] == 0]['cnt'].mean()
-
-# Menampilkan rata-rata penggunaan sepeda pada hari kerja dan hari libur
 st.write('Rata-rata Penggunaan Sepeda pada Hari Kerja: ', average_usage_workday)
 st.write('Rata-rata Penggunaan Sepeda pada Hari Libur: ', average_usage_holiday)
 
